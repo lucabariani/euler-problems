fcf9fd9cff590843bf49b59f99d09621bca63dfa
luca.bariani <lucabariani@gmail.com>
2020-10-26 17:04:28 +0100

    code correct

--- nodejs/problems/problem0719.js
+++ nodejs/problems/problem0719.js
@@ -4,30 +4,54 @@
  */
 
 console.log("pippo");
-T(1000);
+console.log(T(10**4));
+console.log(T(10**12));
 
 
 function T(maxSquare){
 
-    for(let n=9; Math.pow(n,2) < maxSquare; n++){
-        console.log(Math.pow(n,2));
+    let sum = 0;
+    for(let n=9; n ** 2 <= maxSquare; n++){
+        let flag = isSplitEqualTo(n,n**2);
+        //console.log(n,n**2,flag);
+        if (flag){
+            sum += n**2;
+        }
     }
+    return sum;
 }
 
-function isSquareSnumber(n){
-    let square = Math.pow(n,2);
-    return false;
-}
+function isSplitEqualTo(sumOfDigits, digitsToSummarize){
 
-function isSplitEqualTo(nSum, subN){
-
-    if(nSum === subN){
+    //console.log(sumOfDigits, digitsToSummarize);
+    if(sumOfDigits === digitsToSummarize){
+        //console.log(sumOfDigits, digitsToSummarize,"aa");
         return true;
     }
-    if(nSum > subN){
+    if(sumOfDigits > digitsToSummarize){
+        //console.log(sumOfDigits, digitsToSummarize,"bb");
         return false;
     }
+
+    if(sumOfDigits < 0){
+        //console.log(sumOfDigits, digitsToSummarize,"dd");
+        return false;
+    }
+
     // else
-    let tmp = subN.toString();
-    let first = str.substring(0, 1);
+    const digitsMinus1 = Math.floor(Math.log10(digitsToSummarize));
+    if (digitsMinus1 === 0){
+        //console.log(sumOfDigits, digitsToSummarize,"cc");
+        return false;
+    }
+
+    for (let lessSignificantDigit = digitsMinus1; lessSignificantDigit > 0; lessSignificantDigit--) {
+        const mostSignificantDigits = Math.floor(digitsToSummarize / (10 ** lessSignificantDigit));
+        const newDigitsToSummarize = digitsToSummarize - mostSignificantDigits * (10 ** lessSignificantDigit);
+        const newSumOfDigits = sumOfDigits - mostSignificantDigits;
+        if(isSplitEqualTo(newSumOfDigits, newDigitsToSummarize)){
+            return true;
+        }
+    }
+    return false;
 }
